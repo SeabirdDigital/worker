6853c1727a87c8e95dcc1c5d371a4c069f2d762b
Filip Mårtensson <filip.martensson@gtg.se>
2022-12-27 00:18:24 +0100

    Switch do default export

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,12 +1,12 @@
-import { credential, initializeApp } from 'firebase-admin';
+import admin from 'firebase-admin';
 import {
 	SECRET_FIREBASE_PROJECT_ID,
 	SECRET_FIREBASE_CLIENT_EMAIL,
 	SECRET_FIREBASE_KEY
 } from '$env/static/private';
 
-initializeApp({
-	credential: credential.cert({
+admin.initializeApp({
+	credential: admin.credential.cert({
 		projectId: SECRET_FIREBASE_PROJECT_ID,
 		clientEmail: SECRET_FIREBASE_CLIENT_EMAIL,
 		privateKey: SECRET_FIREBASE_KEY
