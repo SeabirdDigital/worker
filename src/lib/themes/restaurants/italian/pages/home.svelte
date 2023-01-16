886a811a22e94949235f971f96c4d6f758bcd024
Filip Mårtensson <fenwikk@protonmail.com>
2023-01-16 23:01:49 +0100

    Italian, Reviews

--- src/lib/themes/restaurants/italian/pages/home.svelte
+++ src/lib/themes/restaurants/italian/pages/home.svelte
@@ -2,6 +2,8 @@
 	import Text from '$lib/components/Text.svelte';
 	import ArrowDown from '../components/icons/ArrowDown.svg';
 	import ChevronRight from '../components/icons/ChevronRight.svg';
+	import Quote from '../components/icons/Quote.svg';
+	import Star from '../components/icons/Star.svg';
 </script>
 
 <div class="h-screen w-full flex flex-col">
@@ -43,9 +45,9 @@
 <div class="flex h-[85vh]">
 	<div class="ml-[15%] flex flex-grow">
 		<div class="h-full w-1/2 flex flex-col gap-2 justify-center px-20">
-			<h1 class="text-4xl md:text-4xl 2xl:text-5xl font-lora">
+			<h2 class="text-4xl md:text-4xl 2xl:text-5xl font-lora">
 				<Text id="aboutHeading" />
-			</h1>
+			</h2>
 			<p>
 				<Text id="aboutText" />
 			</p>
@@ -54,3 +56,59 @@
 		<div class="w-1/2 h-full bg-cover" style="background-image: url(/si/about.jpg);" />
 	</div>
 </div>
+
+<div class="flex">
+	<div id="stars" class="w-[15%] flex flex-col items-center py-6">
+		<img src={Star} alt="" />
+		<img src={Star} alt="" />
+		<img src={Star} alt="" />
+		<img src={Star} alt="" />
+	</div>
+
+	<div class="flex flex-col items-center justify-center flex-grow gap-8">
+		<div class="relative">
+			<img class="-z-10 absolute -top-12 -left-16 h-36" src={Quote} alt="" />
+
+			<h2 class="text-4xl md:text-4xl 2xl:text-5xl font-lora">Recensioner</h2>
+		</div>
+
+		<div class="flex text-sm text-center container" id="reviews">
+			<div>
+				<p>
+					Mycket god pizza och servicen var mycket bra.
+					<b>Kocken vet hur man gör riktig italiensk pizza.</b>
+					Tack för pizzan.
+				</p>
+				<span>Mubinul Mulk</span>
+			</div>
+			<div>
+				<p>
+					<b>Bästa pizzan i Malmö. Hands down.</b> Alla ingredienser är färska och alla rätt (som jag
+					smakat) klassar alla andra pizzerior jag har käkat på!
+				</p>
+				<span>Victor Schack</span>
+			</div>
+			<div>
+				<p>
+					Fantastisk pizza. Nya ägaren levererar. <b>En slice räckte</b> för att jag skulle skriva en
+					recension. Vi ses igen.
+				</p>
+				<span>Linus Persson</span>
+			</div>
+		</div>
+	</div>
+</div>
+
+<style lang="postcss">
+	#stars > img {
+		@apply -rotate-45 w-[8vw] h-[8vw] opacity-50;
+	}
+
+	#reviews > div {
+		@apply px-8 flex flex-col gap-4;
+	}
+
+	#reviews span {
+		@apply font-bold;
+	}
+</style>
