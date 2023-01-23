6a6bc950ba3f62bac3bba960ef72f6ab48a32e1c
Filip Mårtensson <filip.martensson@gtg.se>
2023-01-23 12:13:44 +0100

    Nested text

--- src/lib/components/Text.svelte
+++ src/lib/components/Text.svelte
@@ -1,11 +1,19 @@
 <script lang="ts">
 	import pageId from '$lib/stores/pageId';
-
 	import site from '$lib/stores/site';
 
-	export let id: string;
+	export let id: string | string[];
+
+	if (typeof id == 'string') id = id.split('.');
+
+	let text = $site.pages[$pageId][id[0]];
+	for (let i = 0; i < id.length; i++) {
+		if (typeof text != 'object') break;
+
+		text = text[id[i]];
+	}
 </script>
 
 <span data-seabird data-seabird-id={id}>
-	{@html $site.pages[$pageId][id]}
+	{@html text}
 </span>
