559168cbc21e49f341a5e6dde0029b7034fe9e46
Filip Mårtensson <fenwikk@protonmail.com>
2023-04-03 20:40:09 +0200

    Missed one

--- src/routes/portal/edit/+server.ts
+++ src/routes/portal/edit/+server.ts
@@ -1,12 +1,13 @@
 import type { Site } from '$lib/stores/site';
 import type { RequestHandler } from '@sveltejs/kit';
-import { firestore } from 'firebase-admin';
+import admin from 'firebase-admin';
 
 export const POST: RequestHandler = async ({ request }) => {
 	const data: { id: string; site: Site } = await request.json();
 
 	console.log('hi');
-	firestore()
+	admin
+		.firestore()
 		.doc('sites/' + data.id)
 		.update(data.site);
 
