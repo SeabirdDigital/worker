cc6480ebf61b1f85db75cb60c649a5f1e27b2730
Filip Mårtensson <fenwikk@protonmail.com>
2023-06-12 08:52:18 +0200

    Redirect in portal layout

--- src/components/portal/Layout.tsx
+++ src/components/portal/Layout.tsx
@@ -1,4 +1,4 @@
-import { type ReactNode } from "react";
+import { useEffect, type ReactNode } from "react";
 import Header from "./Header";
 
 type LayoutProps = {
@@ -6,6 +6,13 @@ type LayoutProps = {
 };
 
 const Layout = ({ children }: LayoutProps) => {
+	useEffect(() => {
+		const host = process.env.VERCEL_URL ?? process.env.DEV_HOST;
+
+		if (host !== "puffins.se")
+			window.location.href = "https://puffins.se/portal";
+	});
+
 	return (
 		<div className="bg-neutral-900 text-white">
 			<Header />
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,32 +1,8 @@
 import { type AppType } from "next/app";
-import { createContext, useState } from "react";
-import "~/styles/globals.css";
 import { api } from "~/utils/api";
 
-type PuffinsState = {
-	siteId: string | undefined;
-};
-
-export const PuffinsContext = createContext<PuffinsState>({
-	siteId: undefined,
-});
-
-const getHost = () => {
-	return (
-		window?.location.hostname ??
-		process.env.VERCEL_URL ??
-		process.env.DEV_HOST
-	);
-};
-
 const MyApp: AppType = ({ Component, pageProps }) => {
-	const [puffins, setPuffins] = useState<PuffinsState>({ siteId: undefined });
-
-	return (
-		<PuffinsContext.Provider value={puffins}>
-			<Component {...pageProps} />
-		</PuffinsContext.Provider>
-	);
+	return <Component {...pageProps} />;
 };
 
 export default api.withTRPC(MyApp);
